b9b0b99c1647d6d0138b477206d95f70402c2683
noney1412 <nonpanpila@gmail.com>
2018-02-04 14:01:34 +0700

    :wrench: write express server

---
+++ workshop2/server.js
@@ -0,0 +1,12 @@
+import express from 'express'
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send("Hello World ! workshop2 : .babelrc & plugin ")
+}
+)
+
+app.listen(3000)
+
+console.log("http://localhost:3000")
