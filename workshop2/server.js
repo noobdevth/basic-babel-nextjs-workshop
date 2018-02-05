b06d373fca1b77c0a05d7e8617373650adbdaf71
noney1412 <nonpanpila@gmail.com>
2018-02-05 14:53:23 +0700

    :tada: Done ! ep4

--- workshop2/server.js
+++ workshop2/server.js
@@ -3,7 +3,7 @@ import express from 'express'
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send("Hello World ! workshop2 : .babelrc & plugin ")
+    res.send("Hello World ! workshop2 : .babelrc")
 }
 )
 
---
+++ workshop3/src/a.js
@@ -0,0 +1,29 @@
+import express from 'express'
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send("Hello World ! workshop3 : plugin & plugin")
+}
+)
+
+app.listen(3000)
+
+console.log("http://localhost:3000")
+console.log("Mission 1 / กำจัด log ด้วย plugin")
+console.log("Log Log Log Log Log Log Log Log Log Log")
+console.log("Log Log Log Log Log Log Log Log Log Log")
+console.log("Log Log Log Log Log Log Log Log Log Log")
+console.log("Log Log Log Log Log Log Log Log Log Log")
+console.log("Log Log Log Log Log Log Log Log Log Log")
+
+/* Mission 2 : กำจัด Comment 
+
+Comment  Comment Comment Comment Comment Comment Comment Comment Comment Comment 
+Comment Comment Comment Comment Comment Comment Comment Comment Comment Comment Comment 
+Comment Comment Comment Comment Comment Comment Comment Comment Comment Comment Comment 
+Comment Comment Comment Comment Comment Comment Comment Comment Comment Comment 
+Comment Comment Comment Comment Comment Comment Comment Comment 
+
+*/
+
